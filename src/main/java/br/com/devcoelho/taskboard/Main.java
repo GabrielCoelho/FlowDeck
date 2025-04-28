d7867c81a46526f7a00447a5b820a17be4519a7d
Gabriel Coelho Soares <gbcoe@outlook.com.br>
2025-04-28 11:20:48 -0300

    chore(ignored): Removing ignored files and adding Spring application

--- src/main/java/br/com/devcoelho/taskboard/Main.java
+++ src/main/java/br/com/devcoelho/taskboard/Main.java
@@ -1,8 +1,10 @@
 package br.com.devcoelho.taskboard;
 
+import org.springframework.boot.SpringApplication;
+
 /** Hello world! */
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    SpringApplication.run(Main.class, args);
   }
 }
